f1b7c10d611ef9719a9f77dc9bb05ce7b18185a9
prince kumar <kumarprince.7999@gmail.com>
2026-08-19 00:28:38 +0530

    config: allow Render frontend CORS

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
     "http://localhost:5173",
-    "https://YOUR-FRONTEND-NAME.onrender.com",
+    "https://ci-cd-detecting-agent-1.onrender.com",
 ],
     allow_credentials=True,
     allow_methods=["*"],
